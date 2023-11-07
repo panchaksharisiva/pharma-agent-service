0ab5d25565315ed365d3d2b9691b5f5c533b3204
panchaksharisiva <panchaksharisiva@gmail.com>
2023-11-07 20:33:33 +0530

    hi

---
+++ src/main/java/com/agent/supplier/PharmaAgentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agent.supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaAgentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaAgentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agent/supplier/PharmaAgentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent.supplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaAgentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
